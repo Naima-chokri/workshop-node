3d5c46750b2d7a394e859afa92fddb5f1d1ce99a
Naima-chokri <chokrinemat@gmail.com>
2024-02-29 23:14:21 +0100

    Initial commit

---
+++ js/data10.js
@@ -0,0 +1,69 @@
+
+
+ const fs = require("fs")
+const allData = require("./allData")
+
+ const addPerson = (id ,fname , lname) => {
+
+      const allData = loadData()
+
+      const duplicatedDate = allData.filter((obj) => {
+        return obj.id === id 
+      })
+
+    //   console.log(duplicatedDate)
+
+      if (duplicatedDate.length == 0) {
+        allData.push({
+            id : id,
+            fname : fname,
+            lname : lname
+         })
+   
+         savaAllData(allData)
+      } else {
+        console.log("ERROR DUPLICATED ID")
+      }
+
+ }
+ ///////////////////////////////////////////////////////////////////////////////
+
+  const loadData = () => {
+    try {
+        const DataJson = fs.readFileSync("data10.json").toString()
+        return  JSON.parse(DataJson)
+    }
+    catch {
+        return []
+    }
+    
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  const savaAllData = (allData) => {
+      const AllDataJson = JSON.stringify(allData)
+      fs.writeFileSync("data10.json" , AllDataJson )
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  const deleteData = (id) => {
+     const allData = loadData()
+
+     const dataToKeep = allData.filter((obj) => {
+        return obj.id !== id 
+       
+     })
+     savaAllData(dataToKeep)
+     
+     console.log("you have already deleted an Item")
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////////
+  module.exports = {
+    addPerson,
+    deleteData
+  }
+
